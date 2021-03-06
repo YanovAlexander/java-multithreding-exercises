0b608cb1cd967035026aebb99fb356c31f858a4a
Oleksandr Yanov <oleksandr.yanov@thomsonreuters.com>
2021-03-06 11:48:34 +0200

    Exercise 4.

--- src/main/java/ua/goit/online/exercise4/Main.java
+++ src/main/java/ua/goit/online/exercise4/Main.java
@@ -1,21 +1,22 @@
 package ua.goit.online.exercise4;
 
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-    private static Object monitor = new Object();
+    private static List<String> list = new LinkedList<>();
 
-    public void method() throws InterruptedException {
-        synchronized (monitor) {
-            System.out.println("Do something");
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000; i++) {
+            list.add(String.valueOf(i));
         }
-    }
 
+        new Thread(() -> {
+            for (String string : list) {
 
-    public void method2() {
-        synchronized (monitor) {
-            System.out.println("Do something");
-        }
+            }
+        }).start();
+        new Thread(() -> list.remove(20)).start();
     }
 }
 
